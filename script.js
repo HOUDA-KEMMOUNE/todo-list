6593baac3b0bfcf810c0de8bf5f126040cb5b664
KEMMOUNE Houda <Houdakemmoune2006@gmail.com>
2023-09-02 11:39:58 +0200

    add the date

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //DOM
-const 
+const inputEl = document.querySelector('.js-todo-input');
+const submitEl = document.querySelector('.js-todo-btn');
+const inPageEl = document.querySelector('.js-todos');
+
+//The array for the todo list
+const todoList = [{
+    name: 'Wake up at 5 AM',
+    day: '05-10-24'
+},
+{
+    name: 'Pray Salat El Fajr',
+    day: '05-10-24'
+},{
+    name: 'Eat breakfast',
+    day: '05-10-24'
+}]
+console.log(todoList);
